d23ec22382980d25d1d28a4198605721908d7b42
ofirf <ofirf@sentinel.com>
2020-08-16 09:20:15 +0300

    create date picker sub component

--- projects/datepicker/src/lib/datepicker/components/days/days.component.ts
+++ projects/datepicker/src/lib/datepicker/components/days/days.component.ts
@@ -1,6 +1,17 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Inject,
+  Input,
+  OnChanges,
+  Output,
+  Renderer2,
+  SimpleChanges,
+} from '@angular/core';
 import { CalendarDay } from '../../models/calendar-day.interface';
 import { OutputEventInterface } from '../../models/output-event.interface';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'ngx-days',
@@ -12,17 +23,23 @@ export class DaysComponent implements OnChanges {
   @Input() days: CalendarDay[] = [];
   @Output() selected = new EventEmitter<OutputEventInterface<CalendarDay>>();
 
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {}
+
   ngOnChanges(changes: SimpleChanges): void {
     setTimeout(() => {
-      Array.from(document.querySelectorAll('.day-value')).forEach((element: HTMLElement) => {
+      Array.from(this.document.querySelectorAll('.day-value')).forEach((element: HTMLElement) => {
         const dateInMs = parseInt(element.getAttribute('data-date-in-ms'));
-        if (dateInMs >= 1596661200000 && dateInMs <= 1597006800000) {
-          element.classList.add('range-day-marker');
-          if (dateInMs === 1596661200000) {
-            element.classList.add('range-day-marker-start');
-          }
-          if (dateInMs === 1597006800000) {
-            element.classList.add('range-day-marker-end');
+        const startSelectedDate = 1596661200000;
+        const endSelectedDate = 1597006800000;
+
+        if (dateInMs >= startSelectedDate && dateInMs <= endSelectedDate) {
+          this.renderer.addClass(element, 'range-day-marker');
+          if (dateInMs === startSelectedDate) {
+            this.renderer.addClass(element, 'range-day-marker-start');
+          } else if (dateInMs === endSelectedDate) {
+            this.renderer.addClass(element, 'range-day-marker-end');
+          } else {
+            // NOTHING
           }
         }
       });
