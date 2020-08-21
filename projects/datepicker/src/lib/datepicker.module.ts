97b78252fb7d7b9c8939fd2183554a7de605bfd0
ofirf <ofirf@sentinel.com>
2020-08-21 16:53:25 +0300

    create date picker sub component

--- projects/datepicker/src/lib/datepicker.module.ts
+++ projects/datepicker/src/lib/datepicker.module.ts
@@ -5,10 +5,18 @@ import { DaysComponent } from './datepicker/components/days/days.component';
 import { WeekDaysHeaderComponent } from './datepicker/components/week-days-header/week-days-header.component';
 import { DaysHeaderComponent } from './datepicker/components/days-header/days-header.component';
 import { CalendarFooterComponent } from './datepicker/components/calendar-footer/calendar-footer.component';
+import { CalendarComponent } from './datepicker/components/calendar/calendar.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [DatepickerComponent, DaysComponent, WeekDaysHeaderComponent, DaysHeaderComponent, CalendarFooterComponent],
+  declarations: [
+    DatepickerComponent,
+    DaysComponent,
+    WeekDaysHeaderComponent,
+    DaysHeaderComponent,
+    CalendarFooterComponent,
+    CalendarComponent,
+  ],
   exports: [DatepickerComponent],
 })
 export class DatepickerModule {}
--- projects/datepicker/src/lib/datepicker/datepicker/datepicker.component.ts
+++ projects/datepicker/src/lib/datepicker/datepicker/datepicker.component.ts
@@ -1,8 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { getDate, getDateInMs, getNowDate } from '../date.utils';
-import { createCalendarDays } from '../utils/calendar-days.utils';
-import { CalendarDay } from '../models/calendar-day.interface';
-import { IDate } from '../models/date.interface';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-datepicker',
@@ -10,72 +6,4 @@ import { IDate } from '../models/date.interface';
   styleUrls: ['./datepicker.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DatepickerComponent implements OnInit {
-  days: CalendarDay[] = [];
-  currentViewDate: IDate = getNowDate();
-  selectedDate;
-  hidePrevAndNextDays = true;
-  readonly today = getDateInMs(this.currentViewDate.year, this.currentViewDate.month, this.currentViewDate.day);
-
-  ngOnInit(): void {
-    this.createDays(this.currentViewDate.year, this.currentViewDate.month);
-  }
-
-  updateSelected({ event, data: day }: { event: MouseEvent; data: CalendarDay }): void {
-    this.selectedDate = {
-      dateInMs: day.dateInMs,
-      ...getDate(new Date(day.dateInMs)),
-    };
-    this.createDays(2020, this.currentViewDate.month);
-  }
-
-  prevMonth(): void {
-    if (this.currentViewDate.month === 1) {
-      this.currentViewDate.month = 12;
-      this.currentViewDate.year--;
-    } else {
-      this.currentViewDate.month--;
-    }
-
-    this.createDays(2020, this.currentViewDate.month);
-  }
-
-  nextMonth(): void {
-    if (this.currentViewDate.month === 12) {
-      this.currentViewDate.month = 1;
-      this.currentViewDate.year++;
-    } else {
-      this.currentViewDate.month++;
-    }
-    this.createDays(2020, this.currentViewDate.month);
-  }
-
-  showToday(): void {
-    this.currentViewDate = getNowDate();
-    this.createDays(this.currentViewDate.year, this.currentViewDate.month);
-  }
-
-  showSelected(): void {
-    this.currentViewDate = {
-      year: this.selectedDate.year,
-      month: this.selectedDate.month,
-      day: this.selectedDate.day,
-    };
-    this.createDays(this.selectedDate.year, this.selectedDate.month);
-  }
-
-  clear(): void {
-    this.selectedDate = null;
-    this.createDays(this.currentViewDate.year, this.currentViewDate.month);
-  }
-
-  apply(): void {}
-
-  cancel(): void {}
-
-  createDays(year: number, month: number): void {
-    this.days = createCalendarDays(year, month, this.hidePrevAndNextDays, this.selectedDate);
-  }
-
-  hover(): void {}
-}
+export class DatepickerComponent {}
